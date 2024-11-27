10bd94c9b71064c6b1b292b6c170e73d26ca96df
Beatriz Machado <bialmachado@gmail.com>
2024-11-27 16:33:20 -0300

    Done without bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.destroy_all
+
+Flat.create!(
+  name: 'First Flat',
+  address: '1 Avenue',
+  description: 'A lovely flat',
+  price_per_night: 100,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'Second Flat',
+  address: '2 Avenue',
+  description: 'A cozy flat',
+  price_per_night: 50,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Third Flat',
+  address: '3 Avenue',
+  description: 'A modern flat.',
+  price_per_night: 150,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Fourth Flat',
+  address: '4 Avenue',
+  description: 'A luxurious flat.',
+  price_per_night: 200,
+  number_of_guests: 8
+)
